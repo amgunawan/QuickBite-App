0eacc357b01bab3a1c8768c8df9be93bcbf60dc9
Angela Melia Gunawan <iniiangelaa@gmail.com>
2025-10-28 13:59:10 +0700

    Initial Commit

---
+++ QuickBite App/QuickBite_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickBite_AppApp.swift
+//  QuickBite App
+//
+//  Created by Angela on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickBite_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickBite AppTests/QuickBite_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickBite_AppTests.swift
+//  QuickBite AppTests
+//
+//  Created by Angela on 28/10/25.
+//
+
+import Testing
+@testable import QuickBite_App
+
+struct QuickBite_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

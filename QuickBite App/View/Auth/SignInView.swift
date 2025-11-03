08c21f509f303887fd9ac4a3bffaec027b4f9ba2
Angela Melia Gunawan <iniiangelaa@gmail.com>
2025-11-03 16:22:23 +0700

    Folder

---
+++ QuickBite App/View/Auth/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  QuickBite
+//
+//  Created by Angela on 03/11/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}

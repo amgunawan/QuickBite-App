31696eeb272a1bc23f7f8a90d09f83bb151eaa86
Angela Melia Gunawan <iniiangelaa@gmail.com>
2025-11-04 11:33:50 +0700

    Food Category Banner

---
+++ QuickBite App/View/User/Tab Bar/ProfileView.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileView.swift
+//  QuickBite App
+//
+//  Created by jessica tedja on 02/11/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
---
+++ QuickBite App/View/User/Tab Bar/QuestView.swift
@@ -0,0 +1,15 @@
+//
+//  QuestView.swift
+//  QuickBite App
+//
+//  Created by jessica tedja on 02/11/25.
+//
+
+import SwiftUI
+
+struct QuestView: View {
+    var body: some View {
+        Text("Quest View")
+    }
+}
+

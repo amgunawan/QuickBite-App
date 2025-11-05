8196c18de0201a7fe9a474194ce00e154ed6157a
Angela Melia Gunawan <iniiangelaa@gmail.com>
2025-11-05 16:34:09 +0700

    Food Category Button

--- QuickBite App/View/User/Product/FoodCategoryView.swift
+++ QuickBite App/View/User/Product/FoodCategoryView.swift
@@ -11,9 +11,48 @@ struct FoodCategoryView: View {
     let categoryName: String
     
     var body: some View {
-        NavigationStack {
+        VStack (alignment: .leading, spacing: 24) {
+            ScrollView(.vertical, showsIndicators: false) {
+                let columns = [
+                    GridItem(.flexible(), spacing: 16),
+                    GridItem(.flexible(), spacing: 16)
+                ]
+                
+                LazyVGrid(columns: columns, spacing: 16) {
+                    AllRestaurantsCardView(
+                        imageName: "KayaBoys",
+                        deliveryTime: "<10 min",
+                        name: "Kaya Boys"
+                    )
+                    
+                    AllRestaurantsCardView(
+                        imageName: "KPatats",
+                        deliveryTime: "<10 min",
+                        name: "King Patats"
+                    )
+                    
+                    AllRestaurantsCardView(
+                        imageName: "MadameLiy",
+                        deliveryTime: "10–20 min",
+                        name: "Madame Liy"
+                    )
+                    
+                    AllRestaurantsCardView(
+                        imageName: "Raburi",
+                        deliveryTime: "10–20 min",
+                        name: "Raburi"
+                    )
+                }
+            }
             
+            Spacer()
         }
+        .frame(maxWidth: .infinity, alignment: .top)
+        .padding(.horizontal)
         .navigationTitle(categoryName)
     }
+}
+
+#Preview {
+    DeleteAccountView()
 }
